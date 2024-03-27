37085c60ef685d599edd9593ee95b74e369c2eb9
delcin-raj <delcinraj@gmail.com>
2024-03-27 22:52:50 +0530

    day2 session

--- rust-for-bitcoiners-1/day2/demo/src/main.rs
+++ rust-for-bitcoiners-1/day2/demo/src/main.rs
@@ -7,24 +7,56 @@ enum MResult<T, E> {
     Err(E),
 }
 
-impl<T> MResult<T, ()> {
+struct IntList {
+    value: i32,
+    next: Box<IntList> // reference, smart pointer and we know it's size
 }
 
-impl<String, E> MResult<String, E> {
+// Sum and products
+
+enum Expr {
+    Integer(u32), // 256 bits -> [discriminant] [value]
+    Add(Box<Expr>, Box<Expr>), // 
+    Sub(Box<Expr>, Box<Expr>), // 2
+}
+
+/*
+B extends A
+C extends A
+
+Which means A could be A or B or C
+ */
+
+impl<T> MResult<T, String> {
+}
+
+impl<E> MResult<String, E> {
+    fn only_for_string_ok(&self) -> String {
+        // This method does not have the ownership of the self, so it cannot
+        // return the reference as a value to caller
+        match self {
+            MResult::Ok(value) => value.clone(), // duplicate the value
+            MResult::Err(_) => "Error encountered".to_owned(),
+        }
+    }
 }
 
 impl<T, E> MResult<T, E> {
     fn ok(value: T) -> Self {
+        // associated function
         MResult::Ok(value)
     }
     // Function to create an Err variant
     fn err(error: E) -> Self {
-        todo!()
+        MResult::Err(error)
     }
 
     // Method to check if it's an Ok variant
     fn is_ok(&self) -> bool {
-        todo!()
+        match self {
+            MResult::Ok(_) => true,
+            _ => false,
+        }
     }
 
     // Method to check if it's an Err variant
@@ -34,12 +66,16 @@ impl<T, E> MResult<T, E> {
 
     // Method to unwrap the Ok value, panics if it's an Err
     fn unwrap(self) -> T {
+        // Why in the input I'm taking the complete ownership of self?
         todo!()
     }
 
     // Method to unwrap the Err value, panics if it's an Ok
     fn unwrap_err(self) -> E {
-        todo!()
+        match self {
+            MResult::Err(err) => err,
+            _ => panic!("Not an error"),
+        }
     }
 }
 // self, &self and &mut self
@@ -55,12 +91,13 @@ impl Point {
         println!("point {:?} no longer exists", self);
     }
 
+    // In OOP you only have the &mut self variant
     fn set_x(&mut self, new_x: i32) {
-        todo!()
+        self.x = new_x;
     }
 
     fn get_y(&self) -> i32 {
-        todo!()
+        self.y
     } 
 }
 
@@ -86,10 +123,20 @@ trait Shape {
 
 impl Shape for Square {
     fn area(&self) -> u32 {
-        todo!()
+        self.side * self.side
     }
     fn perimeter(&self) -> u32 {
         self.side * 4
+    }
+}
+
+impl Shape for Circle {
+    fn area(&self) -> u32 {
+        3 * self.radius * self.radius
+    }
+
+    fn perimeter(&self) -> u32 {
+        2 * 3 * self.radius
     }
 }
 
@@ -115,9 +162,19 @@ impl u32 {
 */
 
 fn print_area<T: Shape>(shape: T) {
-    todo!()
+    println!("Area of the shape is {}", shape.area());
 }
 
+/*
+fn print_area_square(square: Square) {
+
+}
+fn print_area_circle(circle: Circle) {
+
+}
+ */
+
+// Dynamic dispatch using Vtables, this is executed during runtime
 fn print_perimeter(shape: &dyn Shape) {
     println!("Perimeter: {}", shape.perimeter());
 }
@@ -133,6 +190,7 @@ enum List<T> {
         next: Box<List<T>>
     }
 }
+// 1 -> 2 -> 3 == List::Node {1, Node {2, Node 3, List::Empty}}.push(0) == 0 -> 1 -> 2 -> 3
 
 impl <T> List<T> {
     fn empty() -> List<T> {
@@ -141,16 +199,46 @@ impl <T> List<T> {
     fn singleton(value: T) -> List<T> {
         List::Node { data: value, next: Box::new(List::Empty) }
     }
+
+    fn prepend(self: List<T>, value: T) -> List<T> {
+        List::Node{data: value, next: Box::new(self)}
+    }
 }
 
 
 fn main() {
-    println!("{:?}", MResult::<i32, String>::ok(34));
+    // println!("{:?}", MResult::<i32, String>::ok(34));
 
-    let p = Point{x: 1, y: 2};
-    p.lost();
-    // println!("{:?}", p); Hence not Object oriented
+    // let res: MResult<i32, String> = MResult::ok(94);
+    // println!("{}", res.is_ok());
 
+    // let res: MResult<i32, String> = MResult::err("String".to_owned());
+    // println!("{}", res.is_ok());
+
+    // let p = Point{x: 1, y: 2};
+    // p.lost();
+    // let p = Point{x: 1, y: 2};
+    // // println!("{:?}", p); Hence not Object oriented
+    // println!("{:?}", p.get_y());
+
+    let square = Square {
+        center: Point{x: 0, y : 2},
+        side: 5,
+    };
+    let circle = Circle {
+        center: Point{x: 0, y : 2},
+        radius: 50,
+    };
+
+    print_area(square);
+    print_area(circle);
+
+    let items = vec![1,2,3,4];
+    let mut items_1 = Vec::new();
+    for i in 1..5 {
+        items_1.push(i);
+    }
+    assert_eq!(items, items_1);
 }
 
 // Write tests for List below
